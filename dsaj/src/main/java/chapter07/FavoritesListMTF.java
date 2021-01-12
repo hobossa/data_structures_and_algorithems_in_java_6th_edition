22097325d7fb12f8fa95059c1b13d58a9f1725f5
hobossa <hoboss@gmail.com>
2021-01-12 18:55:59 +0800

    Create FavoritesListMTF.java

---
+++ dsaj/src/main/java/chapter07/FavoritesListMTF.java
@@ -0,0 +1,47 @@
+package chapter07;
+
+/**
+ * Maintains a list of elements ordered with move-to-front heuristic.
+ */
+public class FavoritesListMTF<E> extends FavoritesList<E> {
+
+    /**
+     * Moves accessed item at Position p to the front of the list.
+     */
+    @Override
+    protected void moveUp(Position<Item<E>> p) {
+        if (p != list.first()) {
+            list.addFirst(list.remove(p));
+        }
+    }
+
+    @Override
+    public Iterable<E> getFavorites(int k) throws IllegalArgumentException {
+        if (k < 0 || k > size()) {
+            throw new IllegalArgumentException("Invalid k");
+        }
+
+        // we begin by making a copy of the original list
+        PositionalList<Item<E>> temp = new LinkedPositionalList<>();
+        for (Item<E> item : list) {
+            temp.addLast(item);
+        }
+
+        // we repeated find, report, and remove element with largest count
+        PositionalList<E> result = new LinkedPositionalList<>();
+        for (int i = 0; i <k; i++) {
+            Position<Item<E>> highPos = temp.first();
+            Position<Item<E>> walk = temp.after(highPos);
+            while (walk != null) {
+                if (count(walk)> count(highPos)) {
+                    highPos = walk;
+                }
+                walk = temp.after(walk);
+            }
+            // we have now found element with highest count
+            result.addLast(value(highPos));
+            temp.remove(highPos);
+        }
+        return result;
+    }
+}
