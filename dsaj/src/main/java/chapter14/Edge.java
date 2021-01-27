e5ce762491f5f5463329c0e7df7af90bed930d7f
hobossa <hoboss@gmail.com>
2021-01-27 17:34:57 +0800

    add Interfaces: Edge.java, Vertex.java and Graph.java

---
+++ dsaj/src/main/java/chapter14/Edge.java
@@ -0,0 +1,8 @@
+package chapter14;
+
+/**
+ * An edge of a graph
+ */
+public interface Edge<E> {
+    E getElement();
+}
---
+++ dsaj/src/main/java/chapter14/Vertex.java
@@ -0,0 +1,11 @@
+package chapter14;
+
+/**
+ * A vertex of a graph
+ */
+public interface Vertex<V> {
+    /**
+     * Returns the element associated with the vertex.
+     */
+    V getElement();
+}
