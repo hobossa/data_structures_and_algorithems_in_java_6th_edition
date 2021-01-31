3e48308d61719eeea2b8511c6fc5c67865bd68bc
hobossa <hoboss@gmail.com>
2021-02-01 04:38:45 +0800

    Update GraphAlgorithms.java
    Using Dijkstra's algorithm to get shortest path

--- dsaj/src/main/java/chapter14/GraphAlgorithms.java
+++ dsaj/src/main/java/chapter14/GraphAlgorithms.java
@@ -4,6 +4,9 @@ import chapter06.LinkedStack;
 import chapter06.Stack;
 import chapter07.LinkedPositionalList;
 import chapter07.PositionalList;
+import chapter09.AdaptablePriorityQueue;
+import chapter09.Entry;
+import chapter09.HeapAdaptablePriorityQueue;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -81,6 +84,17 @@ public class GraphAlgorithms {
         }
     }
 
+    public static <V, E> Map<Vertex<V>, Edge<E>> BFSComplete(Graph<V, E> g) {
+        Set<Vertex<V>> known = new HashSet<>();
+        Map<Vertex<V>, Edge<E>> forest = new HashMap<>();
+        for (Vertex<V> u : g.vertices()) {
+            if (!known.contains(u)) {
+                BFS(g, u, known, forest);
+            }
+        }
+        return forest;
+    }
+
     /**
      * Converts graph g into its transitive closure.
      */
@@ -133,4 +147,81 @@ public class GraphAlgorithms {
         }
         return topo;
     }
+
+
+    /**
+     * finds a shortest path - Dijkstra's algorithm.
+     * we assume that e.getElement() for edge e represents the weight of that edge.
+     */
+    public static <V> Map<Vertex<V>, Integer> shortestPathLengths(Graph<V, Integer> g,
+                                                                  Vertex<V> src) {
+        // d.get(v) is upper bound on distance from src to v
+        Map<Vertex<V>, Integer> d = new HashMap<>();
+        // map reachable v to its d value
+        Map<Vertex<V>, Integer> cloud = new HashMap<>();
+        // pq will have vertices as elements, with d.get(v) as key
+        AdaptablePriorityQueue<Integer, Vertex<V>> pq;
+        pq = new HeapAdaptablePriorityQueue<>();
+        // maps from vertex to its pq locator
+        Map<Vertex<V>, Entry<Integer, Vertex<V>>> pqTokens;
+        pqTokens = new HashMap<>();
+
+        // for each vertex v of the graph, add an entry to the priority queue, with
+        // the source having distance 0 and all others having infinite distance
+        for (Vertex<V> v : g.vertices()) {
+            if (v == src) {
+                d.put(v, 0);
+            } else {
+                d.put(v, Integer.MAX_VALUE);
+            }
+            pqTokens.put(v, pq.insert(d.get(v), v));    // save entry for future updates
+        }
+        // now begin adding reachable vertices to the cloud
+        while (!pq.isEmpty()) {
+            Entry<Integer, Vertex<V>> entry = pq.removeMin();
+            int key = entry.getKey();
+            Vertex<V> u = entry.getValue();
+            cloud.put(u, key);      // this is actual distance to u
+            pqTokens.remove(u);
+            for (Edge<Integer> e : g.outgoingEdges(u)) {
+                Vertex<V> v = g.opposite(u, e);
+                if (cloud.get(v) == null) {
+                    // perform relaxation step on edge(u, v)
+                    int wgt = e.getElement();
+                    if (d.get(u) + wgt < d.get(v)) {    // better path to v ?
+                        d.put(v, d.get(u) + wgt);       // update the distance
+                        pq.replaceKey(pqTokens.get(v), d.get(v));   // update the pq entry
+                    }
+                }
+            }
+        }
+        return cloud;       // this onl includes reachable vertices
+    }
+
+    /**
+     * Reconstructs a shortest-path tree rooted at vertex s, given distance map d.
+     * The tree is represented as a map from each reachable vertex v (other than s)
+     * to the edge e = (u,v) that is used to reach v from its parent u in the tree.
+     */
+    public static <V> Map<Vertex<V>, Edge<Integer>> spTree(
+            Graph<V, Integer> g, Vertex<V> s, Map<Vertex<V>, Integer> d) {
+        Map<Vertex<V>, Edge<Integer>> tree = new HashMap<>();
+        for (Vertex<V> v : d.keySet()) {
+            if (v != s) {
+                for (Edge<Integer> e : g.incomingEdges(v)) {
+                    // consider INCOMING edges
+                    Vertex<V> u = g.opposite(v, e);
+                    int wgt = e.getElement();
+                    if (d.get(v) == d.get(u) + wgt) {
+                        tree.put(v, e);     // edge is used to reach v
+                    }
+                }
+            }
+        }
+        return tree;
+    }
+
+    /**
+     * Minimum Spanning Trees
+     */
 }
